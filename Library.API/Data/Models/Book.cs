f4abc37e9c0992080af05297846b5d6c801f6de9
Szabolcs Peter <Szabolcs_Peter@epam.com>
2021-05-06 10:22:54 +0200

    Web API project/tests added

---
+++ Library.API/Data/Models/Book.cs
@@ -0,0 +1,14 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Library.API.Data.Models
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+    }
+}
